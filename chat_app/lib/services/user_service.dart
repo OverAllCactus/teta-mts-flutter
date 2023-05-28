8ce94b4bc92bc977f5cade825d5d021d639c600d
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2023-05-28 20:40:09 +0300

    Update user_service.dart

--- chat_app/lib/services/user_service.dart
+++ chat_app/lib/services/user_service.dart
@@ -2,13 +2,12 @@ import 'package:uuid/uuid.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserService {
-
-  Future <String?> getUser() async {
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  String? value = preferences.getString('userId');
-  return Future.delayed(Duration.zero, () => value);
+  
+  Future<String?> getUser() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String? value = preferences.getString('userId');
+    return value;
   }
-
 
   String createUser() {
     const uuid = Uuid();
@@ -16,14 +15,14 @@ class UserService {
     return userId;
   }
 
-Future setUser(String userId) async {
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  preferences.setString('userId', userId);
+  Future setUser(String userId) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString('userId', userId);
   }
 
   Future checkUser() async {
     String? userFromService = await getUser();
-    if(userFromService == null) {
+    if (userFromService == null) {
       setUser(createUser());
     }
   }
