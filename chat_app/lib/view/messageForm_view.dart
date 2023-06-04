5fd21b11751ff8dc36b04c061a1b5433b5d8c498
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2023-06-04 19:32:25 +0300

    Update messageForm_view.dart

--- chat_app/lib/view/messageForm_view.dart
+++ chat_app/lib/view/messageForm_view.dart
@@ -2,41 +2,55 @@ import 'package:flutter/material.dart';
 
 import '../services/database_service.dart';
 
-class MessageFormView extends StatelessWidget {
+class MessageFormView extends StatefulWidget {
+  const MessageFormView({super.key, required this.databaseService});
 
-  const MessageFormView({
-    super.key,
-    required this.controller// = TextEditingController()
-  });
+  final DatabaseService databaseService;
+  @override
+  State<MessageFormView> createState() =>
+      _MessageFormState(databaseService: databaseService);
+}
 
-final TextEditingController controller;// = TextEditingController();
+class _MessageFormState extends State<MessageFormView> {
+  _MessageFormState({required this.databaseService});
+
+  final TextEditingController _controller = TextEditingController();
+  final DatabaseService databaseService;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-            children: [
-              Expanded(
-                child: BottomAppBar(
-                  child: TextField(
-                    controller: controller,
-                    style: const TextStyle(fontSize: 16.0),
-                    decoration: const InputDecoration(
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                        labelText: 'Message',
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 8.0)),
-                  ),
+    return Padding(
+        padding: MediaQuery.of(context).viewInsets,
+        child: Row(
+          children: [
+            Expanded(
+              child: BottomAppBar(
+                child: TextField(
+                  controller: _controller,
+                  style: const TextStyle(fontSize: 16.0),
+                  decoration: const InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      labelText: 'Message',
+                      border: OutlineInputBorder(),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0)),
                 ),
               ),
-              IconButton(
-                onPressed: () {
-                  DatabaseService().sendMessage(controller.text);
-                  controller.text = '';
-                },
-                icon: Icon(Icons.send),
-              )
-            ],
-          );
+            ),
+            IconButton(
+              onPressed: () {
+                databaseService.sendMessage(_controller.text);
+                _controller.text = '';
+              },
+              icon: Icon(Icons.send),
+            )
+          ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
