7e0130f2a149a83726de7459a26f6cdda88ba576
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2023-06-14 13:35:54 +0300

    Update settings_page.dart

--- chat_app/lib/pages/settings_page.dart
+++ chat_app/lib/pages/settings_page.dart
@@ -50,7 +50,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Settings'),
+          title: Text('Settings', style: GoogleFonts.oswald().copyWith(color: Colors.blue),),
           actions: [
             _isEdit
                 ? TextButton(onPressed: _done, child: Text('Done'))
