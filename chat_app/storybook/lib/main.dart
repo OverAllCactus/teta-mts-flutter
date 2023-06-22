cfbba8e91c2f15d31abb955c6f43c126b819b8f2
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2023-06-22 18:33:33 +0300

    Update main.dart

--- chat_app/storybook/lib/main.dart
+++ chat_app/storybook/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:chat_app/models/message/message.dart';
+import 'package:chat_app/models/user/user.dart';
+import 'package:chat_app/pages/contacts_page.dart';
 import 'package:chat_app/view/messagesList_view.dart';
+import 'package:chat_app/view/message_view.dart';
+import 'package:chat_app/view/contact_view.dart';
 import 'package:flutter/material.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
@@ -22,19 +26,61 @@ class MyApp extends StatelessWidget {
         plugins: _plugins,
         stories: [
           Story(
-            name: 'List of chats',
-            builder: (context) { 
-              final userId = context.knobs.text(label: 'Id', initial: 'Добро пожаловать!');
-              final text = context.knobs.text(label: 'Text', initial: 'Начните с Вашего первого сообщения...');
-              final timestamp = context.knobs.text(label: 'Time', initial: '0');
+              name: 'List of messages',
+              builder: (context) {
+                final userId = context.knobs
+                    .text(label: 'Id', initial: 'Добро пожаловать!');
+                final text = context.knobs.text(
+                    label: 'Text',
+                    initial: 'Начните с Вашего первого сообщения...');
+                final timestamp =
+                    context.knobs.text(label: 'Time', initial: '0');
 
-              return Center(child: MessagesListView(messageList: [
+                return Center(
+                    child: MessagesListView(messageList: [
                   Message(
                       userId: userId,
                       text: text,
-                      timestamp: int.parse(timestamp))]));
-            }
-          ),
+                      timestamp: int.parse(timestamp)),
+                  Message(
+                      userId: userId,
+                      text: text,
+                      timestamp: int.parse(timestamp))
+                ]));
+              }),
+          Story(
+              name: 'contact',
+              builder: (context) {
+                final id = context.knobs.text(label: 'Id', initial: 'test id');
+                final displayName =
+                    context.knobs.text(label: 'Name', initial: 'test name');
+                final photoUrl = context.knobs.text(label: 'URL', initial: '');
+
+                return Center(
+                    child: ContactView(
+                        user: User(
+                            id: id,
+                            displayName: displayName,
+                            photoUrl: photoUrl)));
+              }),
+          Story(
+              name: 'message',
+              builder: (context) {
+                final userId = context.knobs
+                    .text(label: 'Id', initial: 'Добро пожаловать!');
+                final text = context.knobs.text(
+                    label: 'Text',
+                    initial: 'Начните с Вашего первого сообщения...');
+                final timestamp =
+                    context.knobs.text(label: 'Time', initial: '0');
+
+                return Center(
+                    child: MessageView(
+                        message: Message(
+                            userId: userId,
+                            text: text,
+                            timestamp: int.parse(timestamp))));
+              }),
         ],
       );
 }
