13371d7bb1bd297978488fc27c1b7cfd0bc62e33
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2023-07-12 14:01:43 +0300

    Add files via upload

---
+++ chat_app/lib/pages/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:chat_app/pages/settings_page.dart';
+import 'package:flutter/material.dart';
+
+import 'chats-list_page.dart';
+import 'contacts-list_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPageIndex = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: <Widget>[
+        ContactsListPage(),
+        ChatsListPage(),
+        SettingsPage(),
+      ][currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.people_alt),
+            label: 'Contacts',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          NavigationDestination(
+            // selectedIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
