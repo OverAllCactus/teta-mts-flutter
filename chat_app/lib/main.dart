cbcdf2682b46db1487975db38e39db4d7b0bb80c
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2023-07-18 17:34:45 +0300

    Update main.dart

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -57,6 +57,7 @@ class MyApp extends StatelessWidget {
                             flowKey: flowKey,
                             actions: [
                               AuthStateChangeAction<SignedIn>((context, state) {
+                                
                                 Navigator.of(context)
                                     .pushReplacementNamed('/home');
                               })
